ab59c5972912a79f24aecfc9e20699c02e53d6f3
Gabriel <gab.patterson@gmail.com>
2019-12-09 15:34:30 -0500

    pp.route('/venues')

--- app.py
+++ app.py
@@ -177,7 +177,34 @@ def index():
 def venues():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
+  venues=Venue.query.all()
+  venuesDictList=get_child_data(venues)
+  venuesDictListCleaned=[clean_sql_to_dict(venueDictList) for venueDictList in venuesDictList ]
+
+  for venueDictList in venuesDictListCleaned:
+      for show in venueDictList['upcoming_shows']:
+          showQuery=Artist.query.get(show['artist_id'])
+          show['artist_name']=showQuery.name
+          show['artist_image_link']=showQuery.image_link
+          show['start_time']=show['start_time'].strftime("%m/%d/%Y, %H:%M:%S")
+
+  cities={}
+  for venue in venuesDictListCleaned:
+      newVenue={}
+      newVenue['id']=venue['id']
+      newVenue['name']=venue['name']
+      newVenue['num_upcoming_shows']=venue['upcoming_shows_count']
+
+      if venue['city'] not in cities:
+          cities[venue['city']]={}
+          cities[venue['city']]['city']=venue['city']
+          cities[venue['city']]['state']=venue['state']
+          cities[venue['city']]['venues']=[]
+
+
+      cities[venue['city']]['venues'].append(newVenue)
+
+  data2=[{
     "city": "San Francisco",
     "state": "CA",
     "venues": [{
@@ -198,6 +225,8 @@ def venues():
       "num_upcoming_shows": 0,
     }]
   }]
+  data=[v for v in cities.values()]
+
   return render_template('pages/venues.html', areas=data);
 
 @app.route('/venues/search', methods=['POST'])
@@ -410,43 +439,19 @@ def shows():
   # displays list of shows at /shows
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "venue_id": 1,
-    "venue_name": "The Musical Hop",
-    "artist_id": 4,
-    "artist_name": "Guns N Petals",
-    "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-    "start_time": "2019-05-21T21:30:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 5,
-    "artist_name": "Matt Quevedo",
-    "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "start_time": "2019-06-15T23:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-01T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-08T20:00:00.000Z"
-  }, {
-    "venue_id": 3,
-    "venue_name": "Park Square Live Music & Coffee",
-    "artist_id": 6,
-    "artist_name": "The Wild Sax Band",
-    "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-    "start_time": "2035-04-15T20:00:00.000Z"
-  }]
-  return render_template('pages/shows.html', shows=data)
+  shows=Show.query.all()
+  shows=[show.__dict__ for show in shows]
+  for show in shows:
+      venueQuery=Venue.query.get(show['venue_id'])
+      artistQuery=Artist.query.get(show['artist_id'])
+
+      show['venue_name']=venueQuery.name
+      show['venue_image_link']=venueQuery.image_link
+      show['artist_name']=artistQuery.name
+      show['artist_image_link']=artistQuery.image_link
+      show['start_time']=show['start_time'].strftime("%m/%d/%Y, %H:%M:%S")
+
+  return render_template('pages/shows.html', shows=shows)
 
 @app.route('/shows/create')
 def create_shows():
