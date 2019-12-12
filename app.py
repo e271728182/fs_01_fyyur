aca8d4cdab6f7794ebf9022df613021a60ecc6a7
Gabriel <gab.patterson@gmail.com>
2019-12-12 11:59:56 -0500

    @app.route('/shows/create', methods=['POST'])

--- app.py
+++ app.py
@@ -455,21 +455,26 @@ def shows():
 
 @app.route('/shows/create')
 def create_shows():
-  # renders form. do not touch.
-  form = ShowForm()
-  return render_template('forms/new_show.html', form=form)
+    # renders form. do not touch.
+    form = ShowForm()
+    return render_template('forms/new_show.html', form=form)
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
+    show_form = ShowForm(request.form)
+    try:
+        show = Show(
+            artist_id=show_form.artist_id.data,
+            venue_id=show_form.venue_id.data,
+            start_time=show_form.start_time.data
+        )
+        #show.add()
+        # on successful db insert, flash success
+        flash('Show was successfully listed!')
+    except Exception as e:
+        flash('An error occurred. Show could not be listed.')
 
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  return render_template('pages/home.html')
+    return render_template('pages/home.html')
 
 @app.errorhandler(404)
 def not_found_error(error):
